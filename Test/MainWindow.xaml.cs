127cde854933a7787a7a9ce684965e474ab86d06
Wellington N <42657376+Bryceed@users.noreply.github.com>
2022-03-03 06:29:19 -0300

    First time
    First release

---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MicaWPF.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Web.WebView2.Core;
+
+namespace Test
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : MicaWindow
+    {
+        private const string Uri = "https://youtube.com";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WebView.CoreWebView2.Navigate(Uri);
+        }
+    }
+}
